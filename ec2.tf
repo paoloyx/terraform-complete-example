16cc41430504a7ec7d28219af9c8085a2ad15261
Paolo Filippelli <paoloyx@gmail.com>
2020-12-28 15:48:35 +0100

    Adds creation for controller nodes

--- ec2.tf
+++ ec2.tf
@@ -24,9 +24,30 @@ resource "aws_instance" "workers" {
   count = 3
 }
 
+resource "aws_instance" "controllers" {
+  ami           = data.aws_ami.ubuntu_20_04_LTS.id
+  instance_type = var.instance_type
+
+  tags = {
+    owner = "paolofilippelli"
+    env   = "develop"
+    app   = "udemy"
+  }
+
+  # Creates three instances
+  count = 3
+}
+
 output "workers_public_ip_addresses" {
   description = "The public ip address of every worker instance"
   value = [
     for worker in aws_instance.workers[*]: "Worker ${worker.id} has ip address ${worker.public_ip}"
+  ]
+}
+
+output "controllers_public_ip_addresses" {
+  description = "The public ip address of every controller instance"
+  value = [
+    for controller in aws_instance.controllers[*]: "Controller ${controller.id} has ip address ${controller.public_ip}"
   ]
 }
