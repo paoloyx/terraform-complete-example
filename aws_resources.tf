e9345649ce810cd90622fad31e550996a4008f6c
Paolo Filippelli <paoloyx@gmail.com>
2020-12-30 19:33:01 +0100

    Factor resources in files with appropriate naming

--- aws_resources.tf
+++ aws_resources.tf
@@ -1,15 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-}
-provider "aws" {
-  region = var.aws_region
-}
-
 # Security group for worker nodes
 resource "aws_security_group" "workers_ssh_access" {
   name = "workers_ssh_access"
@@ -43,18 +31,4 @@ resource "aws_instance" "controllers" {
 
   # Instances count is set by `high_availability` variable
   count = (var.high_availability == true ? 3 : 1)
-}
-
-output "workers_public_ip_addresses" {
-  description = "The public ip address of every worker instance"
-  value = [
-    for worker in aws_instance.workers[*] : "Worker ${worker.id} has ip address ${worker.public_ip}"
-  ]
-}
-
-output "controllers_public_ip_addresses" {
-  description = "The public ip address of every controller instance"
-  value = [
-    for controller in aws_instance.controllers[*] : "Controller ${controller.id} has ip address ${controller.public_ip}"
-  ]
 }
---
+++ outputs.tf
@@ -0,0 +1,13 @@
+output "workers_public_ip_addresses" {
+  description = "The public ip address of every worker instance"
+  value = [
+    for worker in aws_instance.workers[*] : "Worker ${worker.id} has ip address ${worker.public_ip}"
+  ]
+}
+
+output "controllers_public_ip_addresses" {
+  description = "The public ip address of every controller instance"
+  value = [
+    for controller in aws_instance.controllers[*] : "Controller ${controller.id} has ip address ${controller.public_ip}"
+  ]
+}
