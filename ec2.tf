69216b166a3e297eefbed1ac0474b0d91a1c1217
Paolo Filippelli <paoloyx@gmail.com>
2020-12-30 08:41:12 +0100

    Adds a security group to allow ssh access to workers

--- ec2.tf
+++ ec2.tf
@@ -10,10 +10,24 @@ provider "aws" {
   region = var.aws_region
 }
 
+# Security group for worker nodes
+resource "aws_security_group" "workers_ssh_access" {
+  name = "workers_ssh_access"
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+    # TODO: remove global access?
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "workers" {
   ami             = data.aws_ami.ubuntu_20_04_LTS.id
   instance_type   = var.instance_type
   key_name        = "terraform-oregon"
+  security_groups = [aws_security_group.workers_ssh_access.name]
 
   tags = merge(local.common_tags, local.worker_tags)
 
