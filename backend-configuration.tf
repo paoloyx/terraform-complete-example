ce9021f5f6639e62a20348510a6b396bd17c8c75
Paolo Filippelli <paoloyx@gmail.com>
2020-12-31 08:54:37 +0100

    Uses DynamoDB table for remote s3 state locking

--- backend-configuration.tf
+++ backend-configuration.tf
@@ -3,5 +3,6 @@ terraform {
     bucket = "terraform-udemy-complete-example-backend"
     key    = "terraform-state"
     region = "us-west-2"
+    dynamodb_table="terraform-udemy-complete-example"
   }
 }
