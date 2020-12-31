4e50c9a8a872223da432c1a626da20d9dd9d27f7
Paolo Filippelli <paoloyx@gmail.com>
2020-12-31 07:39:01 +0100

    Modifies workers security group

--- aws_resources.tf
+++ aws_resources.tf
@@ -1,6 +1,6 @@
 # Security group for worker nodes
-resource "aws_security_group" "workers_ssh_access" {
-  name = "workers_ssh_access"
+resource "aws_security_group" "workers_security_group" {
+  name = "workers_security_group"
 
   ingress {
     from_port = 22
@@ -9,13 +9,28 @@ resource "aws_security_group" "workers_ssh_access" {
     # TODO: remove global access?
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  ingress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+    # TODO: remove global access?
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_instance" "workers" {
   ami             = data.aws_ami.ubuntu_20_04_LTS.id
   instance_type   = var.instance_type
   key_name        = "terraform-oregon"
-  security_groups = [aws_security_group.workers_ssh_access.name]
+  security_groups = [aws_security_group.workers_security_group.name]
 
   tags = merge(local.common_tags, local.worker_tags)
 
