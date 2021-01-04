df4b6c1272e93018aec4eac2eafcd5d8646408d9
Paolo Filippelli <paoloyx@gmail.com>
2021-01-04 07:59:45 +0100

    Modifies backend to 'remote' terraform cloud

--- backend-configuration.tf
+++ backend-configuration.tf
@@ -1,8 +1,11 @@
+# Using a single workspace:
 terraform {
-  backend "s3" {
-    bucket = "terraform-udemy-complete-example-backend"
-    key    = "terraform-state"
-    region = "us-west-2"
-    dynamodb_table="terraform-udemy-complete-example"
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "paolofilippelli"
+
+    workspaces {
+      name = "terraform-complete-example"
+    }
   }
 }
